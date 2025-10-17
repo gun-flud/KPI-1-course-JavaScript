302b0cc0a01d672d78971177e38f99751e26a2c1
vladIvannikov <ivannikovvladislav1@gmail.com>
2025-10-17 23:56:26 +0300

    Оновлення лаби

--- lab1/lab1.js
+++ lab1/lab1.js
@@ -1,39 +1,58 @@
-'use strict';
+"use strict";
 // Завдання 1
-const alpha = () => {
-    const param = "dkfjlkk";
-    console.log(param);
+myFunc();
+// console.log(param);
+
+function myFunc() {
+  const param = "dkfjlkk";
+  console.log(param);
 };
-alpha();
+// let alert = 1;
+// console.log(alert);
+
 // Завдання 2
 
 // Перше рішення
-const inc1 = (num) =>  {
-    return num + 1;
-};
-const a = 5;
-const b = inc1(a);
-console.log({ a, b });
+// const inc1 = (num) => {
+//   return num + 1;
+// };
+// const a = 5;
+// const b = inc1(a);
+// console.log({ a, b });
 
-//Друге рішення
-const inc2 = (num) => {
-    return ++num.n;
-};
-const obj = { n: 5 };
+// //Друге рішення
+// const inc2 = (num) => {
+//   return ++num.n;
+// };
+// const obj = { n: 5 };
 
-console.log(inc2(obj));
+// console.log(inc2(obj));
 
-// Завдання 3
-const map = [true, 'hello', 5, 12, -200, false, false, 'word'];
-const table = { number: 0, string: 0, boolean: 0 };
+// // Завдання 3
+// const map = [true, "hello", 5, 12, -200, false, false, "word"];
+// const table = {number: 0, string: 0, boolean: 0,};
 
-for ( let i = 0; i < map.length; i++) {
-    if (typeof map[i] === 'number' ) {
-        table.number++;
-    } else if (typeof map[i] === 'string') {
-        table.string++;
-    } else if (typeof map[i] === 'boolean'){
-        table.boolean++;  
-    }
-}
-console.log(table);
+// // for..of
+// // for ( let i = 0; i < map.length; i++) {
+// //     if (typeof map[i] === 'number' ) {
+// //         table.number++;
+// //     } else if (typeof map[i] === 'string') {
+// //         table.string++;
+// //     } else if (typeof map[i] === 'boolean'){
+// //         table.boolean++;
+// //     }
+// // }
+// // for ( let element of map) {
+// //     if (typeof element === 'number' ) {
+// //         table.number++;
+// //     } else if (typeof element === 'string') {
+// //         table.string++;
+// //     } else if (typeof element === 'boolean'){
+// //         table.boolean++;
+// //     }
+// // }
+// for (const element of map) {
+//   let type = typeof element;
+//   table[type]++;
+// }
+// console.log(table);
