fd8eb2768a67c97abebd967537f23561204b1b04
vladIvannikov <ivannikovvladislav1@gmail.com>
2025-11-26 20:42:12 +0200

    final

--- lab6/task2.js
+++ lab6/task2.js
@@ -3,14 +3,30 @@
 // const twice = (x) => x * 2;
 // const cube = (x) => x ** 3;
 
-// const pipe = (...func) => {
-    
-//     pipe.on('error', e => { 
-//        return undefined;
-//      });
-//     return func.reduceRight((acc, value) => value(acc), val);
-// };
+// const pipe = (...func) => typeof i in func === 'number' ? undefined : func.reduceRight((acc, value) => value(acc), val);
+
+
 
 // console.log(pipe(inc, twice, cube));
 // console.log(pipe(inc, inc));
-// console.log(pipe(inc, 7, cube));
+// // console.log(pipe(inc, 7, cube));
+
+const val = 5;
+const inc = (x) => ++x;
+const twice = (x) => x * 2;
+const cube = (x) => x ** 3;
+
+const pipe = (...func) => {
+    try{
+        return func.reduceRight((acc, value) => value(acc), val);
+    }catch (Error) {
+        return undefined;
+    }
+    
+};
+
+
+
+console.log(pipe(inc, twice, cube));
+console.log(pipe(inc, inc));
+console.log(pipe(inc, 7, cube));
