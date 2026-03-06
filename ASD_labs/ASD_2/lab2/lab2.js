67c90de144fd862f4f47dd218efc6f1de73652ef
vladIvannikov <ivannikovvladislav1@gmail.com>
2026-03-06 17:57:36 +0200

    com

--- ASD_labs/ASD_2/lab2/lab2.js
+++ ASD_labs/ASD_2/lab2/lab2.js
@@ -10,10 +10,31 @@ class ListNode {
  * @param {ListNode} head
  * @return {ListNode}
  */
-const values = [];
 
 var sortList = function(head) {
     if (!head) return null;
+
+    let step = 0;
+    let curr = head.next;
+
+    while(curr) {
+        curr = curr.next;
+        step++;
+    }
+
+    curr = head;
+    // for (let i = 1; i < step; i++) {
+
+    // }
+
+    let dummyHead = new ListNode(0);
+
+    while (list1 && list2) {
+        if (list1.val < list1.val) {
+            dummyHead.next = list1;
+            
+        }
+    }
 
            
 };
@@ -48,5 +69,9 @@ const listNodeToArray = (head) => {
 
 
 console.log(listNodeToArray(arrayToListNode([3, 8, 5, 1, 4, 7, 6, 2])));
+3 5 1 4 
+1 3 4 5
+console.log(sortList(arrayToListNode([3, 8, 5, 1, 4, 7, 6, 2])));
+console.log(listNodeToArray(arrayToListNode([])));
 // [-1, 5, 3, 4, 0]
 // [-1, 5, 3, 4, 0, -5, 10, 2]
