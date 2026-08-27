4f60ebcaa3c7b44d6a45c0efa4d9adcd26ca3db0
IVANNYKOV VLADYSLAV <ivannikovvladislav1@gmail.com>
2026-08-27 17:37:33 -0400

    chore(leetcode): 1071. Greatest Common Divisor of Strings

---
+++ leetCode/1071_Greatest_Common_Divisor_of_Strings.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+var gcdOfStrings = function(str1, str2) {
+    if 
+    
+};
+//1 ABA ABAABA -> ABA
+//2 AAA AAAAAA -> AAA
+//3 ABCD ABC -> ""
+
+// length1 length2
